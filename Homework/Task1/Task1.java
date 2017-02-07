df71b8ad77b88d41ae694289695e267fd4c24d72
halka <halka@192.168.1.123>
2017-02-07 22:47:29 +0200

    Improving Tasks 1,2

--- Homework/Task1/Task1.java
+++ Homework/Task1/Task1.java
@@ -9,17 +9,23 @@ import java.util.Scanner;
 public class Task1 {
 
 	public static void main(String[] args) {
-		while (true) {
+		boolean decision = true;
+		Scanner sc = new Scanner(System.in);
+		while (decision) {
 			try {
-				Scanner sc = new Scanner(System.in);
 				System.out.println("Enter two numbers:");
 				double n1 = sc.nextDouble();
 				double n2 = sc.nextDouble();
-				System.out.println("Minimum = " + Math.min(n1, n2)); //calling Math.min function turned out to be better than just comparing two double values
+				System.out.println("Minimum = " + Math.min(n1, n2)+"\nWonna stop?Say yes:"); //calling Math.min function turned out to be better than just comparing two double values
+				String decisionInput = sc.next();				
+				if(decisionInput.toLowerCase().equals("yes") == true){
+					decision = false;
+				}
 			} catch (java.util.InputMismatchException e) { //without catching exceptions, program will be stopped on input non-numeric values or non-integer, separated with not a dot
 				System.out.println("Please, enter a numeric value separated with a dot.");
 			}
 		}
+		sc.close();
 	}
 
 }
--- Homework/Task2/Task2.java
+++ Homework/Task2/Task2.java
@@ -18,25 +18,37 @@ public class Task2 {
 	}
 
 	public static void main(String[] args) {
-		while (true) {
+		boolean decision = true;
+		Scanner sc = new Scanner(System.in);
+		while (decision) {
 			try {
-				Scanner sc = new Scanner(System.in);
 				System.out.println("How many numbers would you like to input?");
 				int numberCount = sc.nextInt();
-				int[] numberMassive = new int[numberCount];
-				System.out.println("Input integer numbers:");
-				for (int i = 0; i < numberCount; i++) {
-					numberMassive[i] = sc.nextInt();
+				if (numberCount > 0) {
+					int[] numberMassive = new int[numberCount];
+					System.out.println("Input integer numbers:");
+					for (int i = 0; i < numberCount; i++) {
+						numberMassive[i] = sc.nextInt();
+					}
+					System.out.println("Type the number your massive should be compared to:");
+					int multiplicity = sc.nextInt();
+					if (multiplicity != 0) {
+						Task2 obj = new Task2();
+						System.out.println("The sum is " + obj.sumCalculating(numberMassive, multiplicity)
+								+ "\nWonna stop?Say yes:");
+						String decisionInput = sc.next();
+						if (decisionInput.toLowerCase().equals("yes") == true) {
+							decision = false;
+						}
+					} else {
+						System.out.println("You can't compare multiplicity to zero. Try with another number.");
+					}
 				}
-				System.out.println("Type the number your massive should be compared to:");
-				int multiplicity = sc.nextInt();
-				Task2 obj = new Task2();
-				System.out.println("The sum is " + obj.sumCalculating(numberMassive, multiplicity));
 			} catch (java.util.InputMismatchException e) {
 				System.out.println("Please, enter a numeric value separated with a dot.");
 			}
 		}
-
+		sc.close();
 	}
 
 }
